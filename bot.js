c54e8a4f62da03516b2224e0595a71150a7e373c
Anton Nikitin <nikitinanton2005@yandex.ru>
2026-09-22 12:59:38 +0300

    Добавлена обработка запуска бота

--- bot.js
+++ bot.js
@@ -1,13 +1,28 @@
-import 'dotenv/config';
 import { Bot } from '@maxhub/max-bot-api';
+import 'dotenv/config';
+import {
+    startWaitingForAccessCode,
+    stopWaitigAccessCode,
+    isWaintigAccessCode
+} from './models/user-state.js'
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
 // Обработчик команды /start
-bot.command('start', (ctx) => ctx.reply('Добро пожаловать!'));
+bot.command('start', (ctx) => {
+    const user = ctx.user;
+
+    // TODO: Добавить пользователя в БД с ролью Неизвестен
+
+    // Переводим польхзователя в режим ожидания кода
+    startWaitingForAccessCode(user.user_id);
+    
+    ctx.reply('Добро пожаловать! Введите код доступа');
+})
 
 // Обработчик для любого другого сообщения
 bot.on('message_created', (ctx) => ctx.reply('Новое сообщение'));
 
 // Запуск бота
 bot.start();
+console.log('Бот запущен');
