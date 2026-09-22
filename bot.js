1269da24329a5fde2c9e0f8197082153169aae2e
Anton Nikitin <nikitinanton2005@yandex.ru>
2026-09-22 13:10:51 +0300

    Добавлено консольное логирование

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ const bot = new Bot(process.env.BOT_TOKEN);
 // Обработчик команды /start
 bot.command('start', (ctx) => {
     const user = ctx.user;
+    console.log(`Пользователь ${user.user_id} запустил бота`);
 
     // TODO: Добавить пользователя в БД с ролью Неизвестен
 
@@ -21,7 +22,10 @@ bot.command('start', (ctx) => {
 })
 
 // Обработчик для любого другого сообщения
-bot.on('message_created', (ctx) => ctx.reply('Новое сообщение'));
+bot.on('message_created', (ctx) => {
+    ctx.reply('Новое сообщение')
+    console.log(`Пользователь ${ctx.user.user_id} написал сообщение`);
+})
 
 // Запуск бота
 bot.start();
--- models/user-state.js
+++ models/user-state.js
@@ -2,10 +2,13 @@ const waitingForAccessCode = new Set();
 
 export function startWaitingForAccessCode(user_id) {
     waitingForAccessCode.add(user_id);
+    console.log(`Пользователь ${user_id}: Ожидание кода доступа`);
 }
 export function stopWaitigAccessCode(user_id) {
     waitingForAccessCode.delete(user_id);
+    console.log(`Пользователь ${user_id}: Ожидание кода доступа - Выход`);
 }
 export function isWaintigAccessCode(user_id) {
     waitingForAccessCode.has(user_id);
+    console.log(`Пользователь ${user_id}: Ожидание кода доступа - Проверка`);
 }
